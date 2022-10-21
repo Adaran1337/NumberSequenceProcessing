6655d16150e44d2eb52138159da1abc5e0528450
Adaran1337 <matveev.aleksandr_1337@mail.ru>
2022-10-21 14:38:30 +0300

    Initial commit

---
+++ src/test/java/com/example/numbersequenceprocessing/NumberSequenceProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.numbersequenceprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberSequenceProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
